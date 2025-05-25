2bcd61a65d6d5c0973b142f1f7d44bbfd1285d8c
Romany Makram Yassa <romany.makram.yassa@gmail.com>
2025-05-25 18:48:22 +0200

    update pages  rebuild

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "862302353"
+    serviceWorkerVersion: "2029411611"
   }
 });
