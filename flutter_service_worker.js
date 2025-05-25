1e75cc0642ae2bcf80e26ebe993ea60ea9cea471
Romany Makram Yassa <romany.makram.yassa@gmail.com>
2025-05-25 17:30:30 +0200

    Deploy portfolio

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,9 +3,13 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"assets/AssetManifest.bin": "d20d13aefa60f8e39e58d4dc53de3da7",
-"assets/AssetManifest.bin.json": "610ec5ac390829399901118bd3ee7530",
-"assets/AssetManifest.json": "e37e3024eaaa2743c8cb4805af3e983b",
+const RESOURCES = {"assets/AssetManifest.bin": "e1e5f171c767e8f98e845a707871011e",
+"assets/AssetManifest.bin.json": "484c92f724f1d3b22cac907613ddbfca",
+"assets/AssetManifest.json": "b7cd9417e141d1c5e3ee00c4ab145ddf",
+"assets/assets/AssetManifest.bin": "d20d13aefa60f8e39e58d4dc53de3da7",
+"assets/assets/AssetManifest.bin.json": "610ec5ac390829399901118bd3ee7530",
+"assets/assets/AssetManifest.json": "e37e3024eaaa2743c8cb4805af3e983b",
+"assets/assets/FontManifest.json": "5a32d4310a6f5d9a6b651e75ba0d7372",
 "assets/assets/icons/cct.png": "4cc920b0493ccca1c7fd1b4cdff5422d",
 "assets/assets/icons/clock.svg": "b9ff6dd2862543c3b9743a455bbffd82",
 "assets/assets/icons/cv.png": "7e2a31575b5b36227b556c29787db1c0",
@@ -22,6 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "d20d13aefa60f8e39e58d4dc53de3da7
 "assets/assets/images/cloudRed.svg": "bda20cdd4888404719f432377a3665f9",
 "assets/assets/images/cloudyBlue.svg": "e9d5e246793cd42bc01f6809d826583d",
 "assets/assets/images/crmy.png": "f424aab9fca39985a6997ab3064a9b2c",
+"assets/assets/NOTICES": "be29a5e19a84130c6e349aebdd22e87a",
 "assets/assets/rive/rain.riv": "91a917f7343f7ac4523955e4ac8ca7f6",
 "assets/FontManifest.json": "5a32d4310a6f5d9a6b651e75ba0d7372",
 "assets/fonts/MaterialIcons-Regular.otf": "6676d37c5ceefbd0af3f291733e0cea8",
@@ -45,7 +50,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "d20d13aefa60f8e39e58d4dc53de3da7
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "c69024207f12cb5ed21a44ee271d9351",
+"flutter_bootstrap.js": "37d24aca4d954a5d7446cb6876100c65",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
